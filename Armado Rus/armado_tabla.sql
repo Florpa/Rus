c833235b18921e9f06cc7bd5c15e98724efee3af
Florpa <65678784+Florpa@users.noreply.github.com>
2020-10-22 15:15:36 -0300

    round 2

---
+++ Armado Rus/armado_tabla.sql
@@ -0,0 +1,21 @@
+with
+calendario as(
+			SELECT pk_fecha, dia_semana, 
+			        dia_semana_nombre,dia, mes,feriado,cuatrimestre, trimestre
+            FROM telecom.calendario_2020
+            order by fecha),
+franjas_horarias as (
+            select franja,descripcion
+            from 
+            transitabilidad.franjas_horarias
+            order by franja),
+comercios as (
+            select distinct (categoria), null::float as pond
+            from transitabilidad.comercios_rus)
+					
+
+select * 
+from calendario,
+franjas_horarias,comercios
+where categoria not in ('en_obra','S/D','"comercial_neg"')
+order by pk_fecha,franja, categoria
